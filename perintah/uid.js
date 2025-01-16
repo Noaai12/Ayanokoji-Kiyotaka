c308be946430bd7538746530c53d903f18db0a7f
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-16 22:59:40 +0700

    Hady Zen

--- perintah/uid.js
+++ perintah/uid.js
@@ -4,17 +4,19 @@ module.exports = {
     penulis: "Hady Zen", 
     kuldown: 10,
     peran: 0,
-    tutor: "<kosong/tag>"
+    tutor: "<kosong/reply/tag>"
   }, 
   
   Ayanokoji: async function ({ api, event, args }) {
     if (args[0]) {
   const { mentions } = event;
 	let hadi = ''; 
-		for (const id in mentions) { 
+for (const id in mentions) { 
   hadi += `${mentions[id].replace("@", "")}: ${id}\n`;
-	}
+}
   api.sendMessage(hadi, event.threadID, event.messageID);
+    } else if (event.messageReply.senderID) {
+  api.sendMessage(event.messageReply.senderID, event.threadID, event.messageID); 
     } else { 
   api.sendMessage(event.senderID, event.threadID, event.messageID);
   }
