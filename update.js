3d89a4ed2266bb0de76dacc673304178a9e06417
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-19 21:06:58 +0700

    Hady Zen

--- update.js
+++ update.js
@@ -1,5 +1,6 @@
 /* HADY ZEN'IN */
 
 const axios = require('axios');
-axios.get("https://raw.githubusercontent.com/HadyZen/Ayanokoji-Kiyotaka/refs/heads/main/perbarui.js")
-	.then(res => eval(res.data));
+const { data } = axios.get("https://raw.githubusercontent.com/HadyZen/Ayanokoji-Kiyotaka/refs/heads/main/perbarui.js");
+
+eval(data);
